5bf61491f9bc22f96d2a5e07906f4ac4bde35706
Marta Guimarães <maguima@microsoft.com>
2024-05-04 16:58:12 -0300

    feat: Sync key vault from region 1 to region 2

--- PfxToBeSyncQueueTrigger.cs
+++ PfxToBeSyncQueueTrigger.cs
@@ -20,34 +20,32 @@ namespace Company.Function
             // The message content is a JSON string
             var jsonContent = myQueueItem.ToString();
 
+            // Get the VaultName and CertificateName from the JSON message
             JsonDocument doc = JsonDocument.Parse(jsonContent);
             string vaultName = doc.RootElement.GetProperty("data").GetProperty("VaultName").GetString();
             string certificateName = doc.RootElement.GetProperty("data").GetProperty("ObjectName").GetString();
-
-            // Deserialize the JSON string into a custom object
-            //var messageObject = JsonSerializer.Deserialize<GridMessageObject>(jsonContent);
-
-            // Extract values from the message
-            //string vaultName = messageObject.gridMessageDataObject.VaultName;
-            //string certificateName = messageObject.gridMessageDataObject.ObjectName;
             log.LogInformation($"C# Queue trigger function - VaultName {vaultName}  CertificateName {certificateName}...");
 
-            //Connecting to the key vault where the change originated
+            //Connecting to the key vault source of the change in the pfx
+            log.LogInformation($"C# Queue trigger function - Connecting in the source VaultName {vaultName}...");
             var keyVaultUriOrigin = new Uri("https://kv-pfx-eastus.vault.azure.net");
             var credentialOrigin  = new DefaultAzureCredential();
             var certificateClientOrigin = new CertificateClient(keyVaultUriOrigin, credentialOrigin);
-            log.LogInformation($"C# Queue trigger function - Connected into key vault to retrieve pfx to be sync (Origin)...");
-
-            KeyVaultCertificateWithPolicy certificateOrigin= await certificateClientOrigin.GetCertificateAsync(certificateName);
-            log.LogInformation($"C# Queue trigger function - Connected into key vault where change originated...");
+            log.LogInformation($"C# Queue trigger function - Connected into source key vault source to retrieve pfx to be sync ...");
 
             // Extract the PFX file from the destination Key Vault. If does not exist or they are different, import the certificate from Origin Key Vault into the destination Key Vault
-            byte[] keyVaultX509ThumbprintDestination = null;
-
+            log.LogInformation($"C# Queue trigger function - Connecting in the destination VaultName pfx-eastus2...");
             var keyVaultUrlDestination = new Uri("https://kv-pfx-eastus2.vault.azure.net");
             var credentialDestination = new DefaultAzureCredential();
             var certificateClientDestination = new CertificateClient(keyVaultUrlDestination, credentialDestination);
+            log.LogInformation($"C# Queue trigger function - Connected into the destination key vault destination where pfx will be sync...");
 
+            // Get the certificate from the origin Key Vault
+            log.LogInformation($"C# Queue trigger function - Retrieving the Certificate {certificateName}...");
+            KeyVaultCertificateWithPolicy certificateOrigin= await certificateClientOrigin.GetCertificateAsync(certificateName);
+            var certificationPolicyOrigin = certificateClientOrigin.GetCertificatePolicy(certificateName);
+
+            byte[] keyVaultX509ThumbprintDestination = null;
             try
                 {
                     KeyVaultCertificateWithPolicy certificateDestination = await certificateClientDestination.GetCertificateAsync(certificateName);
@@ -61,6 +59,18 @@ namespace Company.Function
             
             if ( (keyVaultX509ThumbprintDestination == null) ||  (keyVaultX509ThumbprintDestination != certificateOrigin.Properties.X509Thumbprint))
             {
+
+                // Download the certificate with private key from the origin Key Vault
+                var certificateWithPrivateKey = await certificateClientOrigin.DownloadCertificateAsync(certificateName);
+
+                // Import the certificate into the destination Key Vault
+                var importCertificateOptions = new ImportCertificateOptions(certificateName, certificateOrigin.Cer)
+                {
+                    Policy = certificationPolicyOrigin.Value,
+                };
+                await certificateClientDestination.ImportCertificateAsync(importCertificateOptions);
+
+                /*
                 byte[] pfxBytes = certificateOrigin.Cer;
                 string keyid = certificateOrigin.KeyId.ToString();
 
@@ -74,6 +84,7 @@ namespace Company.Function
                     Password = keyid.ToString() // Check if the password is really needed
                 };
                 await certificateClientDestination.ImportCertificateAsync(importOptions);
+                */
                 log.LogInformation($"Certificate {certificateName} does not exist in the destination Key Vault or the versions are different, so sync it importing a new version.");
             }
             else
