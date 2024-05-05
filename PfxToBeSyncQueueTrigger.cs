4b636021d03786f4ff2a9525f373e20becbc3072
Marta Guimarães <maguima@microsoft.com>
2024-05-04 22:10:45 -0300

    Refactor private key extraction from certificate

--- PfxToBeSyncQueueTrigger.cs
+++ PfxToBeSyncQueueTrigger.cs
@@ -54,7 +54,7 @@ namespace Company.Function
                     KeyVaultCertificateWithPolicy certificateDestination = await certificateClientDestination.GetCertificateAsync(certificateName);
                     log.LogInformation($"C# Queue trigger function - Connected into key vault to retrieve pfx at destination...");
                     keyVaultX509ThumbprintDestination =  certificateDestination.Properties.X509Thumbprint;
-                } 
+                }
             catch (Azure.RequestFailedException ex) when (ex.Status == 404)
                 {
                     log.LogInformation($"C# Queue trigger function - Certificate {certificateName} does not exist in the destination Key Vault, so sync it importing a new version.");
@@ -62,47 +62,23 @@ namespace Company.Function
             // If the certificate does not exist in the destination Key Vault or the versions are different, import a new version
             if ( (keyVaultX509ThumbprintDestination == null) ||  (keyVaultX509ThumbprintDestination != certificateOrigin.Properties.X509Thumbprint))
             {
-
                 // Download the certificate with private key from the origin Key Vault
-                //log.LogInformation($"C# Queue trigger function - Downloading the certification at the source...");
-                //var certificateWithPrivateKey = await certificateClientOrigin.DownloadCertificateAsync(certificateName);
+                log.LogInformation($"C# Queue trigger function - Downloading the certification at the source...");
+                X509Certificate2 certificateWithPrivateKey = await certificateClientOrigin.DownloadCertificateAsync(certificateName);
 
                 // Extract the PFX file from the certificate
-                log.LogInformation($"C# Queue trigger function - Extracting pfx certificate...");
-                byte[] pfxBytes = certificateOrigin.Cer;
-                // You can then use the PFX value to instantiate a new X509Certificate2 object
-                //var certificate = new X509Certificate2(pfxBytes, (string)null, X509KeyStorageFlags.Exportable);
-
-                log.LogInformation($"C# Queue trigger function - Extracting private key from certificate...");
-                // Now you can access the private key via the 'PrivateKey' property of the 'certificate' object
-                //RSA privateKey = certificate.GetRSAPrivateKey();
+                log.LogInformation($"C# Queue trigger function - Transforming pfx into bytes ...");
+                byte[] pfxBytes = certificateWithPrivateKey.Export(X509ContentType.Pfx);
                 log.LogInformation($"C# Queue trigger function - pfx: {pfxBytes.ToString}");
-                //log.LogInformation($"C# Queue trigger function - privatekey: {privateKey.ExportRSAPrivateKey().ToString()}");
 
                 // Import the certificate into the destination Key Vault
                 var importCertificateOptions = new ImportCertificateOptions(certificateName, pfxBytes)
                 {
                     Policy = certificationPolicyOrigin.Value,
-                    //Password = privateKey.ExportRSAPrivateKey().ToString(), // Convert privateKey to string
                 };
                 log.LogInformation($"C# Queue trigger function - Importing the certification to the destination...");
                 await certificateClientDestination.ImportCertificateAsync(importCertificateOptions);
                 log.LogInformation($"C# Queue trigger function - Certification synched from Primary Region with Secondary region successfully!");
-                /*
-                byte[] pfxBytes = certificateOrigin.Cer;
-                string keyid = certificateOrigin.KeyId.ToString();
-
-                log.LogInformation($"C# Queue trigger function - File went to bytes...\n");
-                log.LogInformation($"The file in bytes: {pfxBytes.ToString().Length}");
-                log.LogInformation($"The key: {keyid.ToString().Length}");
-
-                // Import the PFX file into the Key Vault as a certificate
-                var importOptions = new ImportCertificateOptions(certificateName, pfxBytes)
-                {
-                    Password = keyid.ToString() // Check if the password is really needed
-                };
-                await certificateClientDestination.ImportCertificateAsync(importOptions);
-                */
             }
             else
             {
@@ -111,20 +87,6 @@ namespace Company.Function
             }
         }
     }
-
-    // Custom object to deserialize the JSON message
-
-    public class GridMessageDataObject
-    {
-        public string VaultName { get; set; }
-        public string ObjectName { get; set; }
-    } 
-
-    public class GridMessageObject
-    {
-        public GridMessageDataObject gridMessageDataObject { get; set; }
-    }
-
 }
 
 
